654b12041bce5bcf951ccfff8ac3ec40a3e7eba1
patel mark <pmark2465@gmail.com>
2023-04-07 22:15:11 +0300

    Update Day(7) hangman word guess game.py

--- Day(7) hangman word guess game.py
+++ Day(7) hangman word guess game.py
@@ -5,35 +5,59 @@ Created on Thu Apr  6 13:43:37 2023
 @author: DATA-JOHN
 """
 
-word_list=["ardvark","baboon","camel"]
+
 
 import random
-chosen_word=random.choice(word_list)
+
+# A list of words to choose from
+word_list=["ardvark","baboon","camel"]
+
+# Picks a random word from the list
+chosen_word = random.choice(word_list)
+
+# For debugging purposes, prints the chosen word
 print(chosen_word)
-display=[]
-end_of_game=False
-lives=6
 
+# Initializes the display list with underscores for each letter in the chosen word
+display = []
 for letter in range(len(chosen_word)):
-     display+="_"
-print(display)
- 
-while end_of_game==False:
-   guess=input("choose a letter: ").lower()
+     display += "_"
 
-   for position in range(len(chosen_word)):
-        letter=chosen_word[position]
-        if letter==guess:
-            display[position]=letter
-   print(display)
-   
-   if guess not in chosen_word:
-       lives-=1
-       if lives==0:
-           end_of_game=True
-           print("out of lives")
+# For debugging purposes, prints the initial display list
+print(display)
+
+# Initializes the game loop
+end_of_game = False
+
+# Initializes the number of lives the player has
+lives = 6
+
+# The main game loop
+while not end_of_game:
     
-   if "_" not in display:
-        end_of_game=True
-        print("you win")
+    # Prompts the user to input a letter
+    guess = input("Choose a letter: ").lower()
+
+    # Loops through each letter in the chosen word to see if it matches the user's guess
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            # If the letter matches the user's guess, replace the underscore in the display with the letter
+            display[position] = letter
+    
+    # Prints the updated display list after the user's guess
+    print(display)
+
+    # If the user's guess is not in the chosen word, decrement the number of lives
+    if guess not in chosen_word:
+        lives -= 1
         
+        # If the user has no more lives, the game ends
+        if lives == 0:
+            end_of_game = True
+            print("Out of lives. Game over.")
+    
+    # If there are no more underscores in the display list, the player wins
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
